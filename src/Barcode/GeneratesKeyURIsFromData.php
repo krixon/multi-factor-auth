df3ee1491bc781f05057f1654f11c1f5cb2e38ad
Karl Rixon <karlrixon@gmail.com>
2017-07-21 17:43:40 +0100

    Move barcode data validation to a dedicated method

--- src/Barcode/GeneratesKeyURIsFromData.php
+++ src/Barcode/GeneratesKeyURIsFromData.php
@@ -6,14 +6,17 @@ trait GeneratesKeyURIsFromData
 {
     protected function generateKeyURI(Data $data) : string
     {
+        $this->assertKnownDataType($data);
+
         $parameters = [
             'secret'    => $data->secret(),
             'issuer'    => $data->issuer(),
             'digits'    => $data->digitCount(),
         ];
 
+        $type = 'totp';
+
         if ($data instanceof TimeBasedData) {
-            $type        = 'totp';
             $parameters += [
                 'period'    => $data->windowLength(),
                 'algorithm' => strtoupper($data->algorithm()),
@@ -24,17 +27,23 @@ trait GeneratesKeyURIsFromData
                 'counter'   => $data->counter(),
                 'algorithm' => 'SHA1',
             ];
-        } else {
-            throw new Exception\InvalidData(sprintf(
-                get_class($this) . ' only supports TimeBasedData and EventBasedData (encountered %s)',
-                get_class($data)
-            ));
         }
 
         $queryString = http_build_query($parameters, '', '&', PHP_QUERY_RFC3986);
         $data        = sprintf('otpauth://%s/%s?%s', $type, $this->label($data), $queryString);
 
         return rawurlencode($data);
+    }
+
+
+    private function assertKnownDataType(Data $data)
+    {
+        if (!($data instanceof TimeBasedData || $data instanceof EventBasedData)) {
+            throw new Exception\InvalidData(sprintf(
+                get_class($this) . ' only supports TimeBasedData and EventBasedData (encountered %s)',
+                get_class($data)
+            ));
+        }
     }
 
 
