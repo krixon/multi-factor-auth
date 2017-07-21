b498c01900358c657648bc6d6eba37a74fc493c2
Karl Rixon <karlrixon@gmail.com>
2017-07-21 10:04:25 +0100

    Add tests covering the curl HTTP client

---
+++ tests/Unit/HTTP/CurlClientTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Krixon\MultiFactorAuthTests\Unit\HTTP;
+
+use Krixon\MultiFactorAuth\HTTP\CurlClient;
+use Krixon\MultiFactorAuthTests\TestCase;
+use phpmock\phpunit\PHPMock;
+
+class CurlClientTest extends TestCase
+{
+    use PHPMock;
+
+
+    public function testThrowsExpectedExceptionWhenGenerationFails()
+    {
+        $expectedData = 'FooBar';
+        $curlExec     = $this->getFunctionMock('Krixon\MultiFactorAuth\HTTP', 'curl_exec');
+
+        $curlExec->expects($this->any())->willReturn($expectedData);
+
+        $client     = new CurlClient(false);
+        $actualData = $client->get('http://example.com');
+
+        static::assertSame($expectedData, $actualData);
+    }
+}
