ee65f6ba6dbf959064f469e1b3a3165d738ff616
Karl Rixon <karlrixon@gmail.com>
2017-07-31 19:49:04 +0100

    Add unit tests for bar chart options

--- tests/Unit/Barcode/OptionsTest.php
+++ tests/Unit/Barcode/OptionsTest.php
@@ -7,6 +7,87 @@ use Krixon\MultiFactorAuthTests\TestCase;
 
 class OptionsTest extends TestCase
 {
+    public function testWithForegroundColor()
+    {
+        $options = new Options();
+
+        static::assertNotSame('red', $options->foregroundColor());
+        static::assertSame('red', $options->withForegroundColor('red')->foregroundColor());
+    }
+
+
+    public function testWithBackgroundColor()
+    {
+        $options = new Options();
+
+        static::assertNotSame('red', $options->backgroundColor());
+        static::assertSame('red', $options->withBackgroundColor('red')->backgroundColor());
+    }
+
+
+    public function testWithWidth()
+    {
+        $options = new Options();
+
+        static::assertNotSame(100, $options->width());
+        static::assertSame(100, $options->withWidth(100)->width());
+    }
+
+
+    public function testWithHeight()
+    {
+        $options = new Options();
+
+        static::assertNotSame(100, $options->height());
+        static::assertSame(100, $options->withHeight(100)->height());
+    }
+
+
+    public function testWithSourceCharset()
+    {
+        $options = new Options();
+
+        static::assertNotSame('utf-8', $options->sourceCharset());
+        static::assertSame('utf-8', $options->withSourceCharset('utf-8')->sourceCharset());
+    }
+
+
+    public function testWithTargetCharset()
+    {
+        $options = new Options();
+
+        static::assertNotSame('utf-8', $options->targetCharset());
+        static::assertSame('utf-8', $options->withTargetCharset('utf-8')->targetCharset());
+    }
+
+
+    public function testWithErrorCorrectionLevel()
+    {
+        $options = new Options();
+
+        static::assertNotSame('L', $options->errorCorrectionLevel());
+        static::assertSame('L', $options->withErrorCorrectionLevel('L')->errorCorrectionLevel());
+    }
+
+
+    public function testWithMargin()
+    {
+        $options = new Options();
+
+        static::assertNotSame(2, $options->margin());
+        static::assertSame(2, $options->withMargin(2)->margin());
+    }
+
+
+    public function testWithFormat()
+    {
+        $options = new Options();
+
+        static::assertNotSame('png', $options->format());
+        static::assertSame('png', $options->withFormat('png')->format());
+    }
+
+
     /**
      * @dataProvider validUnionProvider
      *
@@ -16,9 +97,7 @@ class OptionsTest extends TestCase
      */
     public function testUnion(Options $lhs, Options $rhs, Options $expected)
     {
-        $result = $lhs->union($rhs);
-
-        static::assertEquals($expected, $result);
+        static::assertEquals($expected, $lhs->union($rhs));
     }
 
 
@@ -71,6 +150,74 @@ class OptionsTest extends TestCase
             // Format
             [$base, $base->withMargin(1), $base->withMargin(1)],
             [$base->withMargin(1), $base->withMargin(2), $base->withMargin(1)],
+        ];
+    }
+
+
+    /**
+     * @dataProvider validEqualsProvider
+     *
+     * @param Options $lhs
+     * @param Options $rhs
+     * @param bool    $expected
+     */
+    public function testEquals(Options $lhs, Options $rhs, bool $expected)
+    {
+        static::assertEquals($expected, $lhs->equals($rhs));
+    }
+
+
+    public function validEqualsProvider()
+    {
+        $base = new Options();
+
+        return [
+            [$base, $base, true],
+
+            // Width
+            [$base, $base->withWidth(200), false],
+            [$base->withWidth(100), $base->withWidth(100), true],
+
+            // Height
+            [$base, $base->withHeight(200), false],
+            [$base->withHeight(100), $base->withHeight(100), true],
+
+            // EC level
+            [$base, $base->withErrorCorrectionLevel('M'), false],
+            [$base->withErrorCorrectionLevel('L'), $base->withErrorCorrectionLevel('L'), true],
+
+            // Foreground color
+            [$base, $base->withForegroundColor('#ffffff'), false],
+            [$base->withForegroundColor('#ffffff'), $base->withForegroundColor('#ffffff'), true],
+
+            // Background color
+            [$base, $base->withBackgroundColor('#ffffff'), false],
+            [$base->withBackgroundColor('#ffffff'), $base->withBackgroundColor('#ffffff'), true],
+
+            // Format
+            [$base, $base->withFormat('png'), false],
+            [$base->withFormat('png'), $base->withFormat('png'), true],
+
+            // Format
+            [$base, $base->withMargin(1), false],
+            [$base->withMargin(1), $base->withMargin(1), true],
+
+            [
+                $base,
+                new class() extends Options
+                {
+
+                },
+                true
+            ],
+            [
+                new class() extends Options
+                {
+
+                },
+                $base,
+                false
+            ],
         ];
     }
 }
